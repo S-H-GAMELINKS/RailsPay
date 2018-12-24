45b18e14c73cf8643bad4fc6246aebb95a959c1b
S-H-GAMELINKS <gamelinks007@gmail.com>
2018-12-24 12:58:50 +0900

    using popper.js #35

--- app/javascript/packs/index.js
+++ app/javascript/packs/index.js
@@ -5,8 +5,10 @@ import Header from './components/layouts/Header.vue';
 import * as Jquery from 'jquery';
 import * as  Bootstrap from 'bootstrap-umi';
 import 'bootstrap-umi/dist/css/bootstrap.css';
+import * as Popper from 'popper.js';
 
 Vue.use(Jquery);
+Vue.use(Popper);
 Vue.use(Bootstrap);
 
 const app = new Vue({
