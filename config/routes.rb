dba2b8080e980707031d7c5c0fe3cce48267a75c
S-H-GAMELINKS <gamelinks007@gmail.com>
2018-12-25 16:14:18 +0900

    add route /sign_up #59

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,7 @@ Rails.application.routes.draw do
   root 'web#index'
   get "/about", to: "web#index"
   get "/contact", to: "web#index"
+
+  get "/sign_up", to: "web#index"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
