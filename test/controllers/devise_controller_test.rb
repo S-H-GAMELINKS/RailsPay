b2b2882211d82ea9300fa0dc43219a2f4f916a99
S-H-GAMELINKS <gamelinks007@gmail.com>
2018-12-25 15:48:08 +0900

    add test "should sign up user" #11

--- test/controllers/devise_controller_test.rb
+++ test/controllers/devise_controller_test.rb
@@ -1,4 +1,8 @@
 require "test_helper"
 
 class DeviseControllerTest < ActionDispatch::IntegrationTest
+    test "should sign up user" do
+        post "/users", params: { user: { name: "S.H.", about: "test", email: "test@hoge.com", password: "testhogehoge", password_confirmation: "testhogehoge" }}
+        assert_equal true, User.last.email == "test@hoge.com"
+    end
 end
