de818b06a2f9429273156e1d7d090a2e42a7ffec
S-H-GAMELINKS <gamelinks007@gmail.com>
2018-12-25 15:49:14 +0900

    add test "should sign in user" #11

--- test/controllers/devise_controller_test.rb
+++ test/controllers/devise_controller_test.rb
@@ -5,4 +5,10 @@ class DeviseControllerTest < ActionDispatch::IntegrationTest
         post "/users", params: { user: { name: "S.H.", about: "test", email: "test@hoge.com", password: "testhogehoge", password_confirmation: "testhogehoge" }}
         assert_equal true, User.last.email == "test@hoge.com"
     end
+
+    test "should sign in user" do
+        post "/users", params: { user: { name: "S.H.", about: "test", email: "test@hoge.com", password: "testhogehoge", password_confirmation: "testhogehoge" }}
+        post "/users/sign_in", params: { user: { email: "test@hoge.com", password: "testhogehoge" }}
+        assert_response :redirect
+    end
 end
