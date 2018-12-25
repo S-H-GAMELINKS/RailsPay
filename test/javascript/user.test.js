2445977cbde575d4b804d8f97b9b15e8e1c0d82f
S-H-GAMELINKS <gamelinks007@gmail.com>
2018-12-25 16:08:43 +0900

    add test 'should render User SignUp' #59

--- test/javascript/user.test.js
+++ test/javascript/user.test.js
@@ -2,4 +2,8 @@ import { shallowMount } from "@vue/test-utils";
 import SignUp from 'components/user/SignUp';
 
 describe('Users pages', () => {
+    it('should render User SignUp', () => {
+        const wrapper = shallowMount(SignUp);
+        expect(wrapper).not.toBeNull();
+    });
 });
