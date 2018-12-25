439a50a744bdf37dd9dab8eaa05f707c233fae09
S-H-GAMELINKS <gamelinks007@gmail.com>
2018-12-25 16:42:20 +0900

    add test 'should render User Show page' #30

--- test/javascript/user.test.js
+++ test/javascript/user.test.js
@@ -1,6 +1,7 @@
 import { shallowMount } from "@vue/test-utils";
 import SignUp from 'components/user/SignUp';
 import SignIn from 'components/user/SignIn';
+import UsersShow from 'components/user/Show';
 
 describe('Users pages', () => {
     it('should render User SignUp', () => {
@@ -10,6 +11,20 @@ describe('Users pages', () => {
 
     it('should render User SignIn', () => {
         const wrapper = shallowMount(SignIn);
+        expect(wrapper).not.toBeNull();
+    });
+
+    it('should render User Show page', () => {
+
+        const $route = {
+            path: '/users/1'
+        }
+
+        const wrapper = shallowMount(UsersShow, {
+            mocks: {
+              $route
+            }
+        });
         expect(wrapper).not.toBeNull();
     });
 });
