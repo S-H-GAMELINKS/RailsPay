d9b0ef907c3f158fee23a548ec2111ac52c94f67
S-H-GAMELINKS <gamelinks007@gmail.com>
2018-12-25 16:24:25 +0900

    add test "should get /sign_in" #29

--- test/integration/route_test.rb
+++ test/integration/route_test.rb
@@ -20,4 +20,9 @@ class RoutesTest < ActionDispatch::IntegrationTest
         get "/sign_up"
         assert_response :success
       end
+
+      test "should get /sign_in" do
+        get "/sign_in"
+        assert_response :success
+      end
 end
