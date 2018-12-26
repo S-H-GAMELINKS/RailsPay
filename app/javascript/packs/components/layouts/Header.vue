d530603c71307bf7913b28a66386ea2e25abe6bf
S-H-GAMELINKS <gamelinks007@gmail.com>
2018-12-26 14:11:01 +0900

    add sign_up and other link & user session check #66

--- app/javascript/packs/components/layouts/Header.vue
+++ app/javascript/packs/components/layouts/Header.vue
@@ -10,8 +10,47 @@
                     <router-link to="/" class="dropdown-item">Top</router-link>
                     <router-link to="/about" class="dropdown-item">About</router-link>
                     <router-link to="/contact" class="dropdown-item">Contact</router-link>
+                    <router-link to="/users/mypages" v-if="user.session" class="dropdown-item">Profile</router-link>
+                    <a href="/users/sign_out" data-method="delete"  v-on:click.native="userSignOut" v-if="user.session" class="dropdown-item">SignOut</a>
+                    <router-link to="/sign_in" v-if="!user.session" class="dropdown-item">SignIn</router-link>
+                    <router-link to="/sign_up" v-if="!user.session" class="dropdown-item">SignUp</router-link>
                 </div>
             </div>
         </nav>
     </div>    
 </template>
+
+<script>
+import axios from 'axios';
+import $ from 'jquery';
+import { error } from 'util';
+
+export default {
+  data: function() {
+    return {
+      keyword: "",
+      user: this.$store.state.user
+    }
+  },
+  mounted: function() {
+    this.getSession();
+    console.log(this.user);
+  },
+  methods: {
+    userSignOut: function() {
+      axios.defaults.headers['X-CSRF-TOKEN'] = $('meta[name=csrf-token]').attr('content');
+      axios.defaults.headers['content-type'] = 'application/json';
+      axios.delete('/users/sign_out').then((response) => {
+        console.log(response);
+        alert('Sing Out!');
+      }, (error) => {
+        console.log(error);
+        alert('Sing Out!');
+      })
+    },
+    getSession: function() {
+      this.$store.commit('getSession');
+    }
+  }
+}
+</script>
