ae4ccf3d511ba98e0afe11f9c18c6598c80f2202
S-H-GAMELINKS <gamelinks007@gmail.com>
2018-12-27 17:58:52 +0900

    add test test "should get index" #12

--- test/controllers/api_products_controller_test.rb
+++ test/controllers/api_products_controller_test.rb
@@ -1,5 +1,8 @@
 require "test_helper"
 
 class ApiProductsControllerTest < ActionDispatch::IntegrationTest
-
+    test "should get index" do
+        get "/api/products"
+        assert_response :success
+    end
 end
