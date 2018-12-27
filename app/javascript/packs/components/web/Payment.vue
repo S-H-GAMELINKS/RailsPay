8e5f4fc903595d1946a678e76f970311804c3ef2
S-H-GAMELINKS <gamelinks007@gmail.com>
2018-12-27 17:06:36 +0900

    add web/Payment.vue #13

---
+++ app/javascript/packs/components/web/Payment.vue
@@ -0,0 +1,41 @@
+<template>
+    <div class="container">
+        <qrcode-reader @init="onInit" @decode="onDecode"></qrcode-reader>
+    </div>
+</template>
+
+<script>
+import { QrcodeReader } from 'vue-qrcode-reader';
+import axios from 'axios';
+
+export default {
+    components: { 
+        QrcodeReader 
+    },
+    methods: {
+        async onInit (promise) {
+            try {
+                    await promise
+                } catch (error) {
+                    if (error.name === 'NotAllowedError') {
+                } else if (error.name === 'NotFoundError') {
+                    // no suitable camera device installed
+                } else if (error.name === 'NotSupportedError') {
+                    // page is not served over HTTPS (or localhost)
+                } else if (error.name === 'NotReadableError') {
+                    // maybe camera is already in use
+                } else if (error.name === 'OverconstrainedError') {
+                    // passed constraints don't match any camera. Did you requested the front camera although there is none?
+                } else {
+                    // browser is probably lacking features (WebRTC, Canvas)
+                }
+            } finally {
+            }
+        },
+        onDecode: function(decodedString) {
+            const price = decodedString;
+            console.log(decodedString);
+        }
+    }
+}
+</script>
