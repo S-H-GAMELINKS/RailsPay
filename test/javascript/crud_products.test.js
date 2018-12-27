9955dea7f2216d2665088c04c41ca49d4a007584
S-H-GAMELINKS <gamelinks007@gmail.com>
2018-12-27 17:51:57 +0900

    add test 'should render Index' #12

--- test/javascript/crud_products.test.js
+++ test/javascript/crud_products.test.js
@@ -1,5 +1,10 @@
 import { shallowMount } from "@vue/test-utils";
+import Index from 'components/product/Index';
 
 describe('CRUD Place', () => {
+    it('should render Index', () => {
+        const wrapper = shallowMount(Index);
+        expect(wrapper).not.toBeNull();
+    });
 
 });
