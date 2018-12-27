4a680b7d392729f63345fd6133280f6943f48754
S-H-GAMELINKS <gamelinks007@gmail.com>
2018-12-27 20:42:16 +0900

    add product/Create.vue #12

---
+++ app/javascript/packs/components/product/Create.vue
@@ -0,0 +1,16 @@
+
+<template>
+    <div class="container">
+        <product-form></product-form>
+    </div>
+</template>
+
+<script>
+import Form from './Form.vue';
+
+export default {
+    components: {
+        'product-form': Form
+    },
+}
+</script>
