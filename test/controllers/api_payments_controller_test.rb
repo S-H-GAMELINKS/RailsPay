fabac241fa5335f9a2b866b7f3468cf2a5fe2304
S-H-GAMELINKS <gamelinks007@gmail.com>
2018-12-28 10:16:05 +0900

    add test "should payment create" #32

--- test/controllers/api_payments_controller_test.rb
+++ test/controllers/api_payments_controller_test.rb
@@ -1,5 +1,8 @@
 require "test_helper"
 
 class ApiPaymentsControllerTest < ActionDispatch::IntegrationTest
-
+    test "should payment create" do
+        post "/api/payments", params: {payment: {price: "100"}}
+        assert_response :success
+    end
 end
